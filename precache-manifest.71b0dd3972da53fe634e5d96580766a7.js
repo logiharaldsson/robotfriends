8a03bc266db982df5168a4fe69562d8cee00c98a
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2020-02-21 18:31:14 +0000

    Updates

---
+++ precache-manifest.71b0dd3972da53fe634e5d96580766a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a34186aadb2676c519985b1d7450ff",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "93b975b5b38994fb0a9e",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4e9b6a1ccd366f8563fd",
+    "url": "/robotfriends/static/css/main.59409dba.chunk.css"
+  },
+  {
+    "revision": "93b975b5b38994fb0a9e",
+    "url": "/robotfriends/static/js/2.764eef01.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotfriends/static/js/2.764eef01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9b6a1ccd366f8563fd",
+    "url": "/robotfriends/static/js/main.602d2938.chunk.js"
+  },
+  {
+    "revision": "8432a6681c6fc45d179d",
+    "url": "/robotfriends/static/js/runtime-main.05482e96.js"
+  }
+]);
